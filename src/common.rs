82c6f60fff4ca791843b399e35bbdd2a9082b68d
MuYu <65322181+MuYu-Blog@users.noreply.github.com>
2025-06-13 12:10:21 +0800

    common.rs

--- src/common.rs
+++ src/common.rs
@@ -1003,7 +1003,7 @@ fn get_api_server_(api: String, custom: String) -> String {
             return format!("http://{}", s);
         }
     }
-    "https://admin.rustdesk.com".to_owned()
+    "https://rustdeskapi.muyu.love".to_owned()
 }
 
 #[inline]
